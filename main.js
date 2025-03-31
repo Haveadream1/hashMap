a3e70a7e39886cb0d894db5bf949c77196994b71
Haveadream1 <260602david@gmail.com>
2025-03-31 10:50:19 +0200

    refactor: main test

--- main.js
+++ main.js
@@ -2,15 +2,31 @@ import HashMap from "./hashMap.js";
 
 const hashMap = new HashMap();
 
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
+hashMap.set('apple', 'red');
+hashMap.set('banana', 'yellow');
+hashMap.set('carrot', 'orange');
+hashMap.set('dog', 'brown');
+hashMap.set('elephant', 'gray');
+hashMap.set('frog', 'green');
+hashMap.set('grape', 'purple');
+hashMap.set('hat', 'black');
+hashMap.set('ice cream', 'white');
+hashMap.set('jacket', 'blue');
+hashMap.set('kite', 'pink');
+// hashMap.set('lion', 'golden');
+
+console.log(hashMap.hash('apple'));
+console.log(hashMap.hash('banana'));
+
+console.log(hashMap.get('apple'));
+console.log(hashMap.get('aple'));
+
+console.log(hashMap.has('apple'));
+console.log(hashMap.has('aple'));
+
+console.log(hashMap.remove('banana'));
+console.log(hashMap.remove('ban'));
+
+console.log(hashMap.length());
+
+console.log(hashMap.entries())
